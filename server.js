5b5d1218e9b9884a8f76b4dffcb27de1a0a49cba
Darken141 <47373497+Darken141@users.noreply.github.com>
2019-06-07 02:48:50 +0100

    finnish connecting to backend

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ app.get('/', (req, res) => {
 
 app.post('/signin', (req, res) => {
     if(req.body.email === database.users[0].email && req.body.password === database.users[0].password){
-        res.json("success");
+        res.json(database.users[0]);
     } else {
         res.status(400).json('error logging in');
     }
@@ -41,8 +41,7 @@ app.post('/register', (req, res) => {
     database.users.push({
         id: '124',
         name: name,
-        email: email,
-        password: password,
+        email: email,               
         entries: 0,
         joined: new Date()
     })
@@ -74,7 +73,7 @@ app.put('/image', (req, res) => {
         }
     })
     if(!found){
-        res.status(404).json("no such user!");
+        res.status(404).json("no found!");
     }
 })
 
