41a3d94636acd039d584f9c9659c92b56fb2aa03
Darken141 <darken141@gmail.com>
2020-08-10 05:52:00 +0200

    set up .env and DB settings

--- server.js
+++ server.js
@@ -12,19 +12,19 @@ const image = require("./controllers/image");
 require("dotenv").config();
 
 const db = knex({
-  development: {
-    client: "pg",
-    connection: {
-      host: process.env.DB_HOST,
-      user: process.env.DB_USER,
-      password: process.env.DB_PWD,
-      database: process.env.DB_DATABASE,
-    },
-  },
-  production: {
-    client: "pg",
-    connection: process.env.DATABASE_URL + "?ssl=true",
-  },
+  // development: {
+  //   client: "pg",
+  //   connection: {
+  //     host: process.env.DB_HOST,
+  //     user: process.env.DB_USER,
+  //     password: process.env.DB_PWD,
+  //     database: process.env.DB_DATABASE,
+  //   },
+  // },
+  // production: {
+  client: "pg",
+  connection: process.env.DATABASE_URL + "?ssl=true",
+  // },
 });
 
 const app = express();
